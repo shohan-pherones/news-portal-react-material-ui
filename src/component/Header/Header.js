3874ddb86b69b566cbd88ff4a79af1e4b72e5b00
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-11 22:26:00 +0600

    header added

---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="logo">News Portal</h1>
+    </div>
+  );
+};
+
+export default Header;
